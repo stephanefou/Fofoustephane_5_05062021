f1145751fa60c46efa860d555d5b5d0b7b7d1bd2
Stephane Fou <t-fane95@hotmail.fr>
2021-06-23 21:17:36 +0200

    adding interpolation variable in product.js

--- view/produit/produit.js
+++ view/produit/produit.js
@@ -15,16 +15,16 @@ fetch("http://localhost:3000/api/teddies/" + id)
     .then(product => {
         console.log(product)
         document.getElementById("produit").innerHTML += `<div class="div-img-product">
-                                                            <img class="product__photo" src="../../img/teddy_1.jpg" alt="Ours en peluche brun">
+                                                            <img class="product__photo" src="../../img/teddy_1.jpg" alt="Ours en peluche ${product.name}">
                                                         </div>
                                                         <div>
-                                                            <h3 class="title">Ours en peluche Name</h3>
-                                                            <p class="description">product-description</p>
-                                                            <span price></span>
+                                                            <h3 class="title">Ours en peluche ${product.name}</h3>
+                                                            <p class="description">${product.description}</p>
+                                                            <span price>${product.price}</span>
                                                             <div class="form">
                                                                 <div class="color_choice">
-                                                                    <label for="Choix de couleurs pour ours Name">Choix de la couleur : </label>
-                                                                    <select name="Choix de couleurs pour ours Name" id="select_1 ">
+                                                                    <label for="Choix de couleurs pour ${product.name}">Choix de la couleur : </label>
+                                                                    <select name="Choix de couleurs pour ${product.name}" id="select_1 ">
                                                                         <option value="Tan">Tan</option>
                                                                         <option value="Chocolate">Chocolate</option>
                                                                         <option value="Black">Black</option>

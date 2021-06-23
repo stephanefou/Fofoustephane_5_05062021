ca4c3c81e6833dd49977b90841279e4bddd3cbf0
Stephane Fou <t-fane95@hotmail.fr>
2021-06-23 08:29:20 +0200

    Adding docs, card-listing of the first page

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ class Product {
 //On crée une boucle "for of" qui va parcourir le tableau jsonListproduct et pour chaque élément de ce tableau créera une variable jsonProduct qu'on pourra manipuler.
 //Pour chaque produit, on crée un nouvel objet produit avec le json en paramètre
 //"+=" permet de concaténer (ajoute la valeur qui est à droite à la valeur déjà existante, celle de gauche) a(nouvelle valeur)=a+b , a+=b
-fetch('http://localhost:3000/api/teddies/')
+GET('http://localhost:3000/api/teddies/')
     .then( data => data.json())
     .then( jsonListProduct => {
         for ( let jsonProduct of jsonListProduct){
